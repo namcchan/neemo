f25d97f2c712956af45c9bcd47a370bceb41a9eb
nawtr <58523775+nawtr@users.noreply.github.com>
2023-07-06 11:10:41 +0700

    update theme schema

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,22 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { NextUIProvider } from '@nextui-org/react';
 import theme from '@/themes/theme';
+import { ThemeProvider } from 'next-themes';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <NextUIProvider theme={theme}>
-      <Component {...pageProps} />
-    </NextUIProvider>
+    <ThemeProvider
+      defaultTheme="light"
+      attribute="class"
+      value={{
+        light: theme.className,
+        dark: theme.className,
+      }}
+    >
+      <NextUIProvider theme={theme}>
+        <Component {...pageProps} />
+      </NextUIProvider>
+    </ThemeProvider>
   );
 }
 
